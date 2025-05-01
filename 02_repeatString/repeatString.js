d075c6814afdb5c6609dbbda5082974f0a97e8b0
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-01 13:49:08 +0200

    Update
    Exercide Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
 
+if (number < 0){
+    return "ERROR"
+}
+
+let resultat = ""
+for (let i = 0; i < number; i++ ){
+    resultat += string
+}
+return resultat
 };
+
+repeatString(("", 10))
 
 // Do not edit below this line
 module.exports = repeatString;
