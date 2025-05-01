729c44f70e98240eafb7f396f97b2abdd6c97643
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-01 15:18:38 +0200

    Update
    Exercice completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...other) {
+    const newArray = []
+    array.forEach(item => {
+        if (!other.includes(item)) {
+            newArray.push(item)
+        }
+    });
+    return newArray
 };
+
+console.log(removeFromArray([1,2,2,3],"tacos"));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
