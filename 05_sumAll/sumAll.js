8f5caab8e3f7e3b728def5a355673c0ee2a0e5fd
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-01 16:02:32 +0200

    Update
    Exercice Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+if (a < 0 || b < 0 ) return "ERROR"
+if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"
+if (a > b){
+    const invert = a
+    a = b 
+    b = invert
+}
 
+let finalresult = 0
+for (let i = a; i<= b;  i++) {
+ finalresult += i
+}
+
+return finalresult
 };
+
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
