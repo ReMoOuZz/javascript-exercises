e0bc7531db79b0d5124fd0053fe79ab42208b71f
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-03 11:38:24 +0200

    Update
    Exercice Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempOnFahrenheit) {
+  const tempCelcius = ((((tempOnFahrenheit)-32)/1.8))
+  return Math.ceil(tempCelcius*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempOnCelcius) {
+  const tempFahrenheit = (((tempOnCelcius)*1.8)+32)
+  return Math.ceil(tempFahrenheit*10)/10
 };
+
+console.log(convertToFahrenheit(-10));
+
 
 // Do not edit below this line
 module.exports = {
