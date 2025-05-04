e32cc8559622e02dc4033552650e5b84f0dd6026
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-04 11:03:12 +0200

    Update
    Exercice Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const stringCleaned = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "")   
+if (stringCleaned.split("").reverse().join("") === stringCleaned) {
+    return true
+} else {
+    return false
+}    
 };
+console.log(palindromes('Racecar!'));
 
 // Do not edit below this line
 module.exports = palindromes;
