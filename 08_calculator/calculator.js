c3cd321b4aa00fb8590e0a7424fe7a78e8265710
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-05-04 10:13:01 +0200

    Update
    Exercice Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  const result = (a) + (b)
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(c, d) {
+	const result = (c)-(d)
+  return result
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	const result = sumArray.reduce((acc, currentValue)=> {
+    return acc + currentValue
+  }, 0)
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+	const result = multiplyArray.reduce((acc, currentValue)=> {
+    return acc * currentValue
+  },)
+  return result
 };
 
-const power = function() {
-	
+const power = function(e, f) {
+  const result = e ** f
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1
+  }
+  let result = 1
+	for (let i = 1; i <= number; i++) {
+  result *= i }
+
+  return result
 };
+
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
