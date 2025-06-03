2bd98ac400c8dfefa5987562bc830e118b085e92
ReMoOuZz <203637481+ReMoOuZz@users.noreply.github.com>
2025-06-03 10:24:43 +0200

    Exercire completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (typeof n === "string") {
+        n = Number(n)
+    } 
+    
+    if (n === 0) {
+        return 0
+    } else if (!Number.isInteger(n) || n < 0) {
+        return "OOPS"
+    }
+    
+    let a = 1
+    let b = 1
+
+    for (let i = 2; i < n; i++) {
+        
+        const next = a + b;
+        a = b
+        b = next  
+    }
+    return b
 };
+
+console.log(fibonacci("chien"))
 
 // Do not edit below this line
 module.exports = fibonacci;
